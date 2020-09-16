5f9997992b3c794ea087c9cf7b43186b1ab18f73
Mariam-Ismael <65224611+Mariam-Ismael@users.noreply.github.com>
2020-09-16 10:51:31 +0100

    Update form.js

--- form_validation/form.js
+++ form_validation/form.js
@@ -1,7 +1,7 @@
 function validate(){
     let presentDate= new Date();
     let start= new Date(2020, 9, 7);
-    let end= new Date(2020, 9, 11, 11, 59, 59);
+    let end= new Date(2020, 9, 31, 11, 59, 59);
     if(presentDate >= start && presentDate <= end){
         alert('Congratulations! Registration successful.');
         return true;
